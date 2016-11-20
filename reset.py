8a799ef32f149cd11cf74f3b437e4121ea42c2c2
Cameron Sinko <csinko@gmail.com>
2016-11-20 00:17:01 -0500

    Working Script to avrdude to leonardo

---
+++ reset.py
@@ -0,0 +1,12 @@
+import serial, sys
+serialPort = sys.argv[1]
+ser = serial.Serial(
+        port=serialPort,
+        baudrate=1200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+        )
+ser.isOpen()
+ser.close()
+
